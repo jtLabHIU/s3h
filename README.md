2f30707782ac3860787759c6cb245eb716b5b4e6
TANAHASHI, Jiro <jt@do-johodai.ac.jp>
2019-09-17 14:39:40 +0900

    first commit

--- README.md
+++ README.md
@@ -3,3 +3,10 @@ jtLab Scratch 3 Helper app
 - work on Electron
 - run in task tray
 - communicate with Scratch 3 through WebSocket
+
+## How to run
+- `npm install -g electron`
+- `git clone https://github.com/jtLabHIU/s3h.git`
+- `cd s3h`
+- `npm install`
+- `electron .`
---
+++ src/startup.js
@@ -0,0 +1,43 @@
+const {app, BrowserWindow, Menu, Tray} = require('electron');
+
+let mainWindow = null;
+
+app.on('window-all-closed', function() {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('ready', function() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    'min-width': 640,
+    'min-hight': 480,
+    'accept-first-mouse': true,
+    icon: './asset/icon.png'    
+  });
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
+
+  var appIcon = null;
+  appIcon = new Tray('./asset/icon.png');
+  var contextMenu = Menu.buildFromTemplate([
+      { label: 'Restore', type: 'radio' }
+  ]);
+  appIcon.setToolTip('Electron.js App');
+  appIcon.setContextMenu(contextMenu);
+
+  appIcon.on('click', () => {
+    mainWindow.isVisible() ? mainWindow.hide() : mainWindow.show()
+  });
+  mainWindow.on('show', () => {
+    appIcon.setHighlightMode('always')
+  });
+  mainWindow.on('hide', () => {
+    appIcon.setHighlightMode('never')
+  });
+});
